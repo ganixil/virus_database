a74d9fdc8ea0f17fc3ce4953fc80f4ad3c724e3c
xiangli <xiangli1997@gmail.com>
2018-11-28 12:59:03 -0500

    back to string

--- add_virus.py
+++ add_virus.py
@@ -1,8 +1,6 @@
-with open('virus_base.txt','ab') as f:
-	byte = b'\n\t\t# Anythi'
+with open('virus_base.txt','w') as f:
+	byte = '\n\t\t# Anythi'
 	f.write(byte)
-	byte = b'\x03\x00\x00\x80\x02\x00\x00\x00\x0f\x00\x00'
-
+	byte = '\x03\x00\x00\x80\x02\x00\x00\x00\x0f\x00\x00'
 	f.write(byte)
-
 f.close()
